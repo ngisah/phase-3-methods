964b84c474aa4cd64eddbef2522149fff6bbb933
edwin <ngisahedwin@gmail.com>
2022-10-18 14:31:51 +0300

    commit tree d9882c0901fa1eaf9578dd727960da18c9d51d2e

--- methods.rb
+++ methods.rb
@@ -1,1 +1,39 @@
-# Your code here!
+# greet_programmer
+
+def greet_programmer
+    puts "Hello, programmer!"
+end
+greet_programmer
+
+# greet method
+
+def greet name
+    puts "Hello, #{name}!"
+end
+greet "Naureen"
+
+# default param
+
+def greet_with_default(name="programmer")
+    puts "Hello, #{name}!"
+end
+greet_with_default
+greet_with_default "Sunny"
+
+# add method
+def add(num1, num2)
+    num1 + num2
+end
+add(2,2)
+
+#halve interger
+def halve number
+    if number.class != Integer
+        return nil
+    end
+    number / 2
+
+end
+
+puts halve 4
+puts halve"two"
